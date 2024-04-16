661d826873ef9945f4a72b29e160ca42b2268d0a
Arnoldas Mk <arnoldas.kanobi@gmail.com>
2024-04-16 10:30:43 +0300

    stfu

--- c++/8 paskaita.cpp
+++ c++/8 paskaita.cpp
@@ -15,73 +15,78 @@
 // }
 
 
-void funkcija(int* masyvas, int dydis, int& min, int& max) 
+// void funkcija(int* masyvas, int dydis, int& min, int& max) 
+// {
+//     min = max = masyvas[0];
+//     for (int i = 1; i < dydis; i++) 
+//     {
+//         if (masyvas[i] < min) 
+//         {
+//             min = masyvas[i];
+//         }
+//         if (masyvas[i] > max) 
+//         {
+//             max = masyvas[i];
+//         }
+//     }
+// }
+
+// int main() 
+// {
+//     int n;
+//     std::cout << "1-20";
+//     std::cin >> n;
+//     int min, max;
+//     int* masyvas = new int[n];
+
+//     for (int i = 0; i < n; i++) 
+//     {
+//         masyvas[i] = rand() % 201 - 100;
+//     }
+
+//     funkcija(masyvas, n, min, max);
+//     std::cout << "Maziausia: " << min << "\n" << "Didziausia: " << max << std::endl;
+
+// }
+
+
+int funkcija(int* masyvas, int dydis) 
 {
-    min = max = masyvas[0];
-    for (int i = 1; i < dydis; i++) 
+    int n = 0;
+    for (int i = 0; i < dydis; i++) 
     {
-        if (masyvas[i] < min) 
-        {
-            min = masyvas[i];
-        }
-        if (masyvas[i] > max) 
-        {
-            max = masyvas[i];
-        }
-    }
+        if (masyvas[i] > 0) n++;
+    }   
+    return n;
 }
 
-int main() 
-{
+int main() {
+
     int n;
     std::cout << "1-20";
     std::cin >> n;
-    int min, max;
+
     int* masyvas = new int[n];
 
     for (int i = 0; i < n; i++) 
     {
-        masyvas[i] = rand() % 201 - 100;
+        masyvas[i] = rand() % 201 - 100; 
     }
 
-    funkcija(masyvas, n, min, max);
-    std::cout << "Maziausia: " << min << "\n" << "Didziausia: " << max << std::endl;
+    int teigiami_n = funkcija(masyvas, n); 
 
+    int* teigiami = new int[teigiami_n]; 
+
+    for (int i = 0, j = 0; i < n; i++) 
+    {
+        if (masyvas[i] > 0) {
+            teigiami[j++] = masyvas[i];
+        }
+    }
+
+    std::cout << "Teigiami" << std::endl;
+    for (int i = 0; i < teigiami_n; i++) 
+    {
+        std::cout << teigiami[i] << " ";
+    }
 }
-
-
-// int funkcija(int* masyvas, int dydis) {
-//     int n = 0;
-//     for (int i = 0; i < dydis; i++) {
-//         if (masyvas[i] > 0) n++;
-//     }   
-//     return n;
-// }
-
-// int main() {
-
-//     int n;
-//     std::cout << "1-20";
-//     std::cin >> n;
-
-//     int* masyvas = new int[n];
-
-//     for (int i = 0; i < n; i++) {
-//         masyvas[i] = rand() % 201 - 100; 
-//     }
-
-//     int teigiami_n = funkcija(masyvas, n); 
-
-//     int* teigiami = new int[teigiami_n]; 
-
-//     for (int i = 0, j = 0; i < n; i++) {
-//         if (masyvas[i] > 0) {
-//             teigiami[j++] = masyvas[i];
-//         }
-//     }
-
-//     std::cout << "Teigiami" << std::endl;
-//     for (int i = 0; i < teigiami_n; i++) {
-//         std::cout << teigiami[i] << " ";
-//     }
-// }
--- c++/9 paskaita.cpp
+++ c++/9 paskaita.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <string>
 
-bool funkcija(char zodis[])
+bool funkcija(char inputas_zodis)
 {
     bool rastas = false;
     int i = 0;
@@ -16,60 +17,33 @@ bool funkcija(char zodis[])
         i++;
     }
 }
-// int main()
-// {
-//     char inputas_eilute[30];
-//     char inputas_zodis[10] = {};
-//     int eilutes_index = 0;
-//     int zodzio_index = 0;
-//     bool baik = false;
-//     std::cin.get(inputas_eilute, 29);
-//     while (baik == false)
-//     {
-//         if (inputas_eilute[eilutes_index] != ' ' && inputas_eilute[eilutes_index] != '\0')
-//         {
-//             inputas_zodis[zodzio_index] = inputas_eilute[eilutes_index];
-//             zodzio_index++;
-//             eilutes_index++;
-//         }
-//         else
-//         {
-//             inputas_zodis[zodzio_index] = '\0';
-//             std::cout << inputas_zodis << '\n';
-//             if (inputas_eilute[eilutes_index] == '\0')
-//             {
-//                 baik = true;
-//             }
-//             zodzio_index = 0;
-//             eilutes_index = 0;
-//         }
-//     }
-// }
 int main()
 {
-    char Eilute[30];
-    char zodis[10] = {};
-    int e_ind = 0, z_ind = 0;
-    bool pabaiga = false;
-    cout << "Iveskite teksto eilute: \n";
-    cin.get(Eilute, 29);
-    cout << "\nIsvedame zodzius po viena:\n";
-    while (pabaiga == false)
+    char inputas_eilute[30];
+    char inputas_zodis[10] = {};
+    int eilutes_index = 0;
+    int zodzio_index = 0;
+    bool baik = false;
+    std::cin.get(inputas_eilute, 29);
+    while (baik == false)
     {
-        if (Eilute[e_ind] != ' ' && Eilute[e_ind] != '\0') // Tikriname, ar simbolis nėra tarpas
-        {                                                  // bei eilutės pabaigos simbolis
-            zodis[z_ind] = Eilute[e_ind];                  // Kopijuojame po 1 simbolį į masyvą zodis
-            e_ind++;
-            z_ind++;
+        if (inputas_eilute[eilutes_index] != ' ' && inputas_eilute[eilutes_index] != '\0')
+        {
+            inputas_zodis[zodzio_index] = inputas_eilute[eilutes_index];
+            zodzio_index++;
+            eilutes_index++;
         }
         else
         {
-            zodis[z_ind] = '\0'; // Masyvo zodis gale įterpiame eilutės pabaigos simbolį
-            cout << zodis << ‘\n';
-            if (Eilute[e_ind] == '\0’)
-            pabaiga = true;
-            e_ind++; z_ind = 0; //Masyvo zodis indeksavimą pradedame iš naujo
+            inputas_zodis[zodzio_index] = '\0';
+            // std::cout << inputas_zodis << '\n';
+            std::cout << funkcija(zodis);
+            if (inputas_eilute[eilutes_index] == '\0')
+            {
+                baik = true;
+            }
+            eilutes_index++;
+            zodzio_index = 0;
         }
     }
-    return 0;
 }
