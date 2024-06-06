1621aef5f62af58d56f3c47031c704d8b9c66ace
Arnoldas Mk <arnoldas.kanobi@gmail.com>
2024-06-07 02:04:17 +0300

    chore: Update window size and plot parameters in Python code

--- python/bob.py
+++ python/bob.py
@@ -1,19 +1,39 @@
 import dearpygui.dearpygui as dpg
+from math import sin, cos
 
-# Callback function for the button
-def button_callback(sender, app_data, user_data):
-    input_text = dpg.get_value("input_text")
-    print(f"Button clicked! Input text: {input_text}")
-
-# Creating the main window
 dpg.create_context()
 
-with dpg.window(label="Main Window", width=400, height=300):
-    dpg.add_text("Hello, this is a simple Dear PyGui example.")
-    dpg.add_input_text(label="Enter text here", tag="input_text")
-    dpg.add_button(label="Click Me", callback=button_callback)
+sindatax = []
+sindatay = []
+for i in range(0, 500):
+    sindatax.append(i / 1000)
+    sindatay.append(0.5 + 0.5 * sin(50 * i / 1000))
 
-dpg.create_viewport(title='Simple GUI', width=600, height=400)
+def update_series():
+
+    cosdatax = []
+    cosdatay = []
+    for i in range(0, 500):
+        cosdatax.append(i / 1000)
+        cosdatay.append(0.5 + 0.5 * cos(50 * i / 1000))
+    dpg.set_value('series_tag', [cosdatax, cosdatay])
+    dpg.set_item_label('series_tag', "0.5 + 0.5 * cos(x)")
+
+with dpg.window(label="Tutorial", tag="win"):
+    dpg.add_button(label="Update Series", callback=update_series)
+    # create plot
+    with dpg.plot(label="Line Series", height=400, width=400):
+        # optionally create legend
+        dpg.add_plot_legend()
+
+        # REQUIRED: create x and y axes 
+        dpg.add_plot_axis(dpg.mvXAxis, label="x")
+        dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
+
+        # series belong to a y axis
+        dpg.add_line_series(sindatax, sindatay, label="0.5 + 0.5 * sin(x)", parent="y_axis", tag="series_tag")
+
+dpg.create_viewport(title='Custom Title', width=800, height=600)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
--- python/window.py
+++ python/window.py
@@ -1,21 +1,21 @@
 import numpy as np
 import dearpygui.dearpygui as dpg
-import dearpygui.demo as demo
 
-sindatax = np.linspace(0, 2 * np.pi, 100)
+sindatax = np.linspace(0, 2 * np.pi, 1000)
 sindatay = 0.5 + 0.5 * np.sin(sindatax)
-
+width = 1920
+height = 1080
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=1920, height=1080)
+dpg.create_viewport(title='Custom Title', width=width, height=height)
 
-with dpg.window(label='Example Window'):
-    dpg.add_text('Hello, dearpygui!')
+with dpg.window(label='Example Window', autosize=True):
 
-    with dpg.plot(label="Scatter Series", height=400, width=-1):
+    with dpg.plot(label="Scatter Series", height=height, width=width):  # Modified width and height parameters
         dpg.add_plot_legend()
-        dpg.plot_axis(dpg.mvXAxis, label="x")
-        with dpg.plot_axis(dpg.mvYAxis, label="y"):
-            dpg.add_scatter_series(sindatax, sindatay, label="0.5 + 0.5 * sin(x)")
+        x_axis = dpg.add_plot_axis(dpg.mvXAxis, label="x")
+        y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="y")
+        dpg.add_scatter_series(sindatax, sindatay, label="0.5 + 0.5 * sin(x)", parent=x_axis)
+
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
